658a811e8d9f2150bfcff30278b04ac02d3b80dd
Nathan Edwards <edwardsnj@gmail.com>
2019-01-11 22:55:42 -0500

    Tweak

--- gnc.py
+++ gnc.py
@@ -106,26 +106,3 @@ if __name__ == "__main__":
         print ("Orientation: {0}".format(gnc.orientation()))
         print ("Acceleration: {0}".format(gnc.acceleration()))
         time.sleep(5)
-
-
-if len(sys.argv) < 2:
-    print >>sys.stderr, "power.py <pin> [ <pin> ... ]"
-    sys.exit(1)
- 
-
-for pin in sys.argv[1:]:
-    try:
-        pin = int(pin)
-    except ValueError:
-	continue
-    if abs(pin) not in (4,17,18,22,23,24,25):
-	continue
-    if pin > 0:
-        io.setup(pin, io.OUT)
-        io.output(pin, True)
-    else:
-        io.setup(-pin, io.OUT)
-        io.output(-pin, False)
-
-# time.sleep(5)
-# io.cleanup()
